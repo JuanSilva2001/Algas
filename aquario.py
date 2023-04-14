0decaf599fd5201beaafd5f138a05d1318f5be84
JuanSilva01 <juan.silva@bandtec.com.br>
2023-04-14 18:06:38 -0300

    teste nuvem

--- aquario.py
+++ aquario.py
@@ -2,14 +2,14 @@ import random
 import conectDB
 import datetime
 import time
-import matplotlib.pyplot as plt
+import psutil
+
 def gerar_dados(dados):
     vetor_alarme = [8, 26, 20, 6]
-    vetor_tempo = []
-    vetor_quantidade = []
     vetor_bol=['A', 'A', 'A', 'A']
     sql_insert="insert into dados_aquario (sensor, ph, temperatura, turbidez, oxigenio, data_registro) VALUES "
     tempo_inicial = (time.time())
+    processamento_inicial = psutil.cpu_percent()
     now = datetime.datetime(2023,2,random.randint(1,28),0,0,0)
     vetor_ultima_medidas=[(random.randint(0,8)/10)+7, (random.randint(0,9)/10)+random.randint(0,5)+20, (random.randint(0,9)/10)+random.randint(0,8)+10, (random.randint(0,8)/10)+5]
     for j in range(dados):
@@ -35,12 +35,10 @@ def gerar_dados(dados):
                 vetor_bol[k]='D'
             elif vetor_ultima_medidas[k] <= vetor_restart[k]:
                 vetor_bol[k]='A'
+    conectDB.inserirDados(sql_insert[0:-1]+';')
+    processamento_final = psutil.cpu_percent()
+    print(f"{processamento_final}-{processamento_inicial}={processamento_final-processamento_inicial}")
     tempo_final= (time.time())
-    vetor_tempo.append(tempo_final-tempo_inicial)
-    vetor_quantidade.append(dados)
-    print(f"Finalizado em {str(round(tempo_final-tempo_inicial,2)).zfill(5)} com {dados} dados!")
-    return sql_insert
+    print(f"Finalizado em {str(round(tempo_final-tempo_inicial,2))} com uso de memoria {str(round(processamento_final-processamento_inicial,2))} com {dados} dados!")
 for i in range(5):
-    conectDB.inserirDados(gerar_dados(int(input(f"Quantos dados voce deseja inserir no banco? ({i+1}º cenário)\n").replace("_","")))[0:-1]+';')
-#plt.plot(vetor_tempo, vetor_quantidade)
-#plt.show()
+    gerar_dados(int(input(f"Quantos dados voce deseja inserir no banco? ({i+1}º cenário)\n").replace("_","")))
---
+++ comandos.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt install python3-pip
+pip install mysql-connector-python
+sudo apt install mysql-server
+git clone https://github.com/JuanSilva2001/Algas.git
